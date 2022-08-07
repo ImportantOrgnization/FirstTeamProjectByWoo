32c413cc174fa1290cbc9a7079535d4664ef75bb
woo <engineerwoo@outlook.com>
2022-08-07 23:55:24 +0800

    17.1.4 Green for luma

--- Assets/CustomRP/Shaders/FXAAPass.hlsl
+++ Assets/CustomRP/Shaders/FXAAPass.hlsl
@@ -3,7 +3,8 @@
 
 float GetLuma(float2 uv)
 {
-    return sqrt( Luminance(GetSource(uv)));
+    //return sqrt( Luminance(GetSource(uv)));
+    return GetSource(uv).g; //由于人类眼睛对绿色敏感，所以这是一种更加高效的获取 luma 的方法
 }
 
 float4 FXAAPassFragment (Varyings input) : SV_TARGET {
